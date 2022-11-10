2f9ef0fa41fee0558c7128e9fb18a84756b96fed
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-11-10 18:13:55 +0700

    Fairpool

--- models/Person/spreadName.ts
+++ models/Person/spreadName.ts
@@ -4,7 +4,7 @@ import { fromStringToId } from '../../../generic/models/Id'
 export function spreadName(firstName?: string, lastName?: string, shortname?: string, uid?: string) {
   const $name = (firstName || lastName) ? `${firstName ?? ''} ${lastName ?? ''}`.trim() : undefined
   const $shortname = shortname || firstName
-  const $uid = ensure(uid || ($name && fromStringToId($name)) || (shortname && fromStringToId(shortname)), new Error('Person.uid is required'))
+  const $uid = fromStringToId(ensure(uid || $name || shortname, new Error('Person.uid is required')))
   return {
     uid: $uid,
     name: $name,
