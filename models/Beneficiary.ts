e15eb33af6b101cf3bbda67f34c9d1e74776ef9f
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-06 14:24:31 +0700

    Move

---
+++ models/Beneficiary.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod'
+import { getArraySchema } from 'zenbox-util/zod'
+import { isEqualBy } from 'zenbox-util/lodash'
+import { AddressSchema } from '../../ethereum/models/Address'
+import { UintSchema } from '../../ethereum/models/Uint'
+
+export const BeneficiarySchema = z.object({
+  address: AddressSchema,
+  share: UintSchema,
+}).describe('Beneficiary')
+
+export const BeneficiaryUidSchema = BeneficiarySchema.pick({
+  address: true,
+})
+
+export const BeneficiariesSchema = getArraySchema(BeneficiarySchema, parseBeneficiaryUid)
+
+export type Beneficiary = z.infer<typeof BeneficiarySchema>
+
+export type BeneficiaryUid = z.infer<typeof BeneficiaryUidSchema>
+
+export function parseBeneficiary(beneficiary: Beneficiary): Beneficiary {
+  return BeneficiarySchema.parse(beneficiary)
+}
+
+export function parseBeneficiaries(beneficiarys: Beneficiary[]): Beneficiary[] {
+  return BeneficiariesSchema.parse(beneficiarys)
+}
+
+export function parseBeneficiaryUid(beneficiaryUid: BeneficiaryUid): BeneficiaryUid {
+  return BeneficiaryUidSchema.parse(beneficiaryUid)
+}
+
+export const isEqualBeneficiary = (a: Beneficiary) => (b: Beneficiary) => isEqualBy(a, b, parseBeneficiaryUid)
---
+++ models/Token.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod'
+import { getArraySchema } from 'zenbox-util/zod'
+import { isEqualBy } from 'zenbox-util/lodash'
+import { AddressSchema } from '../../ethereum/models/Address'
+import { UintSchema } from '../../ethereum/models/Uint'
+import { BeneficiariesSchema } from './Beneficiary'
+import { BalancesBNSchema } from '../../ethereum/models/BalanceBN'
+import { NameSchema } from '../../generic/models/Name'
+
+export const TokenSchema = z.object({
+  address: AddressSchema,
+  name: NameSchema,
+  symbol: NameSchema,
+  decimals: UintSchema,
+  owner: AddressSchema,
+  speed: UintSchema,
+  tax: UintSchema,
+  beneficiaries: BeneficiariesSchema,
+  balances: BalancesBNSchema,
+}).describe('Token')
+
+export const TokenUidSchema = TokenSchema.pick({
+
+})
+
+export const TokensSchema = getArraySchema(TokenSchema, parseTokenUid)
+
+export type Token = z.infer<typeof TokenSchema>
+
+export type TokenUid = z.infer<typeof TokenUidSchema>
+
+export function parseToken(token: Token): Token {
+  return TokenSchema.parse(token)
+}
+
+export function parseTokens(tokens: Token[]): Token[] {
+  return TokensSchema.parse(tokens)
+}
+
+export function parseTokenUid(tokenUid: TokenUid): TokenUid {
+  return TokenUidSchema.parse(tokenUid)
+}
+
+export const isEqualToken = (a: Token) => (b: Token) => isEqualBy(a, b, parseTokenUid)
