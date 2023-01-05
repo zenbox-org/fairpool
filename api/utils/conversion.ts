636813ba28e389d56e027d68987cac699e0e2285
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-01-05 20:58:34 +0700

    Fairpool

--- api/utils/conversion.ts
+++ api/utils/conversion.ts
@@ -1,6 +1,6 @@
 import { BN } from '../../../bn'
 import { sqrt } from '../../../bn/sqrt'
 
-export const getQuoteFromBase = (base: BN, speed: BN, scale: BN) => base.mul(speed).div(scale).pow(2)
+export const getQuoteFromBase = (base: BN, speed: BN, scale: BN) => base.mul(base).mul(speed).div(scale)
 
-export const getBaseFromQuote = (quote: BN, speed: BN, scale: BN) => sqrt(quote).mul(scale).div(speed)
+export const getBaseFromQuote = (quote: BN, speed: BN, scale: BN) => sqrt(quote.mul(scale).div(speed))
