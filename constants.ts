5e150535a7fcd74c87fffefab1c3c9a7094892f8
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-01-29 13:52:13 +0700

    Improve Fairpool contract

--- constants.ts
+++ constants.ts
@@ -1,5 +1,4 @@
 import { bn } from '../bn/utils'
-import { DefaultScale as QuoteScale } from '../ethereum/constants'
 
 // updated to 18 after switch to Bancor formula
 export const DefaultDecimals = bn(18)
@@ -8,8 +7,12 @@ export const DefaultScale = bn(10).pow(DefaultDecimals)
 
 export const DefaultRoundingPlaces = 6
 
+export const DefaultSlope = bn(5)
+
 export const WeightDecimals = bn(6)
 
 export const WeightScale = bn(10).pow(WeightDecimals)
 
-export const DefaultQuoteBuffer = QuoteScale.mul(10)
+export const ShareDecimals = bn(6)
+
+export const ShareScale = bn(10).pow(ShareDecimals)
